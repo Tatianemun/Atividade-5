b24ada9d4aa3169319a762d8903b9fbc1afe9df1
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-03-01 11:47:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudatexto(){
+    document.getElementById('demo').innerHTML= "Olá eu sou o JavaScript"
+}
+
+function mostrarHora(){
+    document.getElementById("paragrafo2").innerHTML = Date()
+}
